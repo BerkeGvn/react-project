9f2e67977c5bb6863460a8cbf147e6dce5d78206
Berke <berkegvn@gmail.com>
2024-02-06 15:12:56 +0900

    img res

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,10 @@ export default function App() {
         src="../public/asd.svg"
         alt=""
       />
+      <img
+        src="../public/asd.svg"
+        alt=""
+      />
     </div>
   );
 }
