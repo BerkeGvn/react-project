47977603888905d4e1d24299875c18632712fcd9
Berke <berkegvn@gmail.com>
2024-02-28 15:20:34 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,49 @@
-function App() {}
+import { useState } from 'react';
+
+function App() {
+  const [fullname, setFullname] = useState({
+    name: '',
+    surname: '',
+  });
+
+  function setName(e) {
+    const newName = e.target.value;
+    setFullname((prev) => {
+      return {
+        ...prev,
+        name: newName,
+      };
+    });
+  }
+  function setSurname(e) {
+    const newSurname = e.target.value;
+    setFullname((asd) => {
+      return {
+        ...asd,
+        surname: newSurname,
+      };
+    });
+  }
+
+  return (
+    <>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        onChange={setName}
+      />
+      <input
+        type="text"
+        name="surname"
+        id="surname"
+        onChange={setSurname}
+      />
+      <h1>
+        {fullname.name} {fullname.surname}
+      </h1>
+    </>
+  );
+}
 
 export default App;
