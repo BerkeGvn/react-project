1ce3c528fff80b127ab604cdf82eef003322ae11
Berke <berkegvn@gmail.com>
2024-03-14 17:33:58 +0900

    reset comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,10 @@
 import { useEffect, useState, useRef } from 'react';
 
 function App() {
-  const [value, setValue] = useState('');
-  const [count, setCount] = useState(-1);
-  const test = useRef(0);
-
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [canMove, setCanMove] = useState(true);
-
-  useEffect(() => setCount(count + 1), [count, value]);
-
-  const onChange = ({ target }) => setValue(target.value);
-  test.current++;
-
-  useEffect(() => {
-    function handleMove(e) {
-      console.log(e.clientX, e.clientY);
-      setPosition({ x: e.clientX, y: e.clientY });
-    }
-    window.addEventListener('pointermove', handleMove);
-    return () => window.removeEventListener('pointermove', handleMove);
-  }, []);
   return (
-    <div>
-      <input
-        type="text"
-        value={value}
-        onChange={onChange}
-      />
-      <div>Number of changes: {count}</div>
-      <label>
-        <input
-          type="checkbox"
-          checked={canMove}
-          onChange={(e) => setCanMove(e.target.checked)}
-        />
-        The dot is allowed to move.
-      </label>
-      <div
-        style={{
-          position: 'absolute',
-          backgroundColor: 'red',
-          borderRadius: '50%',
-          opacity: 0.6,
-          transform: `translate(${position.x}px, ${position.y}px)`,
-          pointerEvents: 'none',
-          left: -20,
-          top: -20,
-          width: 40,
-          height: 40,
-        }}
-      />
-    </div>
+    <>
+      <h1>restart</h1>
+    </>
   );
 }
 
