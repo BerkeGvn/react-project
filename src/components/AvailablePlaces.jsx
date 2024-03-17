69c334b36f83945e0efc846df78dc021d22ac902
Berke <berkegvn@gmail.com>
2024-03-17 22:32:50 +0900

    error comp

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,24 +1,49 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setisFetching] = useState(false);
+  const [error, setError] = useState(false);
   const [avaiblePlaces, setAviablePlaces] = useState([]);
 
   useEffect(() => {
+    setisFetching(true);
     async function fetchPlaces() {
-      const response = await fetch('http://localhost:3000/places');
-      const data = await response.json();
-      setAviablePlaces(data.places);
+      try {
+        const response = await fetch('http://localhost:3000/places');
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch places');
+        }
+        setAviablePlaces(data.places);
+      } catch (error) {
+        setError({ message: error.message || 'Failed to Fetch' });
+      }
+
+      setisFetching(false);
     }
 
     fetchPlaces();
   }, []);
 
+  if (error) {
+    return (
+      <Error
+        title={'An Error Occured'}
+        message={error.message}
+      ></Error>
+    );
+  }
+
   return (
     <Places
       title="Available Places"
       places={avaiblePlaces}
+      isLoading={isFetching}
+      isLoadingText={'Fetching the data...'}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -1,11 +1,19 @@
 /* eslint-disable react/prop-types */
-export default function Places({ title, places, fallbackText, onSelectPlace }) {
+export default function Places({
+  title,
+  places,
+  fallbackText,
+  onSelectPlace,
+  isLoading,
+  isLoadingText,
+}) {
   console.log(places);
   return (
     <section className="places-category">
       <h2>{title}</h2>
-      {places.length === 0 && <p className="fallback-text">{fallbackText}</p>}
-      {places.length > 0 && (
+      {isLoading && <p className="fallback-text">{isLoadingText}</p>}
+      {!isLoading && places.length === 0 && <p className="fallback-text">{fallbackText}</p>}
+      {!isLoading && places.length > 0 && (
         <ul className="places">
           {places.map((place) => (
             <li
